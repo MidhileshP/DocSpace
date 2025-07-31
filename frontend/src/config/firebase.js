0e6ce4f2e3c3ca18b666dc689f5c2203aec6326b
midhileshp <124782075+MidhileshP@users.noreply.github.com>
2025-07-31 16:48:52 +0530

    Update firebase.js

--- frontend/src/config/firebase.js
+++ frontend/src/config/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import firebaseConfig from './firebaseConfig.json';
+import firebaseConfig from '/etc/secrets/firebaseConfig.json';
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
